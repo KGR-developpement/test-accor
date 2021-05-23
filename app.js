08dc6ee3df4bb9066ff9b8c8219b002616578990
kevin <kens91000@homtail.fr>
2021-05-23 20:44:39 +0200

    feat(step-1): implement findHotelsNearby

--- app.js
+++ app.js
@@ -4,8 +4,30 @@ const priceService = require('./services/price-service');
 const helper = require('./services/helper');
 
 function findHotelsNearby(lat, lng, radius) {
-    // TODO implement me
-	return [];
+    const convertToRadian = (point) => {
+        return point * Math.PI / 180;
+    }
+    const hotels = hotelService.getHotels();
+    const pointA = {
+        lat: convertToRadian(lat),
+        lng: convertToRadian(lng),
+    }
+    const pointB = {};
+    let sortedHotel = [];
+
+    hotels.forEach((hotel) => {
+        pointB.lat = convertToRadian(hotel.latitude);
+        pointB.lng = convertToRadian(hotel.longitude);
+        let distance = 6371 * Math.acos(Math.sin(pointA.lat) * Math.sin(pointB.lat) + Math.cos(pointA.lat) * Math.cos(pointB.lat) * Math.cos(pointB.lng - pointA.lng));
+        distance = distance * 1000;
+
+        if (distance <= radius) {
+            hotel.distance = Math.round(distance);
+            sortedHotel.push(hotel);
+        }
+    })
+
+    return sortedHotel;
 }
 
 function findHotelNearbyWithBestOffer(lat, lng, radius, date) {
@@ -13,7 +35,9 @@ function findHotelNearbyWithBestOffer(lat, lng, radius, date) {
     return null;
 }
 
+console.log(findHotelsNearby(48.856564, 2.351711, 2000))
+
 module.exports = {
-	findHotelsNearby: findHotelsNearby,
-	findHotelNearbyWithBestOffer: findHotelNearbyWithBestOffer
+    findHotelsNearby: findHotelsNearby,
+    findHotelNearbyWithBestOffer: findHotelNearbyWithBestOffer
 }
